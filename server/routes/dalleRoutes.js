c1c6d0130225c7dd53053e47276b81ce91959409
becomealpha <120771676+becomealpha@users.noreply.github.com>
2023-02-02 09:24:38 -0500

    full stack completion

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,4 +1,4 @@
-import express, { response } from "express";
+import express from "express";
 import * as dotenv from "dotenv";
 import { Configuration, OpenAIApi } from "openai";
 
@@ -13,7 +13,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 router.route("/").get((req, res) => {
-    res.send("Hello from OpenAI API");
+    res.status(200).json({ message: 'Hello from DALL-E!' });
 });
 
 router.route("/").post(async (req, res) => {
